2be12d7fb238aad5d6cbf1057439759f9f3973ad
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-13 13:32:45 +0000

    [Add]chapter7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
-  
-  validates :name, presence: true, 
+
+  validates :name, presence: true,
                      length: { maximum: 50 }
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # rails側でセーブ前に小文字に
   before_save { self.email = self.email.downcase }
-  validates :email, presence: true, 
+  validates :email, presence: true,
                       length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                   uniqueness: true
                   # uniqueness: { case_sensitive: false }
-                  
+
   has_secure_password
-  validates :password, presence: true, 
+  validates :password, presence: true,
                          length: { minimum: 6 }
-  
+
 end
