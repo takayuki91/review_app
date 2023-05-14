62e81dbfccf29c49f541cca7d06e8e13706a42d8
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-14 14:15:20 +0000

    [Add]chapter7-formwith

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,31 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
+  
+  def create
+    # User.create(params[:user])
+  @user = User.new(user_params)
+   if @user.save
+   else
+    render 'new'
+   end
+  end
+  
   
   def show
     @user = User.find(params[:id])
     # debugger
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, 
+                                 :email, 
+                                 :password,
+                                 :password_confirmation)
   end
 
 end
